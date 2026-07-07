b31f4d8826ac912b8129afe649e29d8dbf70715f
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-07-07 23:14:04 +0300

    Changes

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import './Home.css'
+import './Video.css'
 
-const Home = () => {
+const Video = () => {
   return (
     <div>
 
@@ -9,4 +9,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Video
