b6bd22832838cc3cdb64faed79e5fa7edf68b72f
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-07-18 23:35:47 +0530

    Added PlayVideo and recommended component

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,17 +1,78 @@
 import React from 'react'
 import './PlayVideo.css'
 import video1 from '../../assets/video.mp4'
-import like from'../..assets/like.png'
-import dislike from'../..assets/dislike.png'
-import share from'../..assets/share.png'
-import save from'../..assets/save.png'
-import jack from'../..assets/jack.png'
-import user_profile from'../..assets/user_profile.jpg'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+import jack from '../../assets/jack.png'
+import user_profile from'../../assets/user_profile.jpg'
 
 const PlayVideo = () => {
   return (
     <div className='play-video'>
-
+      <video src={video1} controls autoplay muted></video>
+      <h3>Best YouTube Channel To Learn Web Development</h3>
+      <div className='play-video-info'>
+        <p>1525 Views &bull; 2 days ago</p>
+        <div>
+          <span><img src={like} alt="" />125</span>
+          <span><img src={dislike} alt="" />2</span>
+          <span><img src={share} alt="" />Share</span>
+          <span><img src={save} alt="" />Save</span>
+        </div>
+      </div>
+      <hr />
+      < div className="publisher">
+        <img src={jack} alt="" />
+        <div>
+          <p>GreatStack</p>
+          <span>1m Subscribers</span>
+        </div>
+         <button>Subscribe</button>
+      </div>
+      <div className="vid-description">
+        <p>Channel that makes learning easy</p>
+        <p>Subscribe to GreatStack to watch more tutorials on web development</p>
+        <hr />
+        <h4>130 Comments</h4>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Michael Ebenezer <span>1 day ago</span></h3>
+            <p>Clean code, great formatting, and excellent explanations here.</p>
+          </div>
+          <div className="comment-action">
+            <img src={like} alt="" />
+            <span>244</span>
+            <img src={dislike} alt="" />
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Michael Ebenezer <span>1 day ago</span></h3>
+            <p>Clean code, great formatting, and excellent explanations here.</p>
+          </div>
+          <div className="comment-action">
+            <img src={like} alt="" />
+            <span>244</span>
+            <img src={dislike} alt="" />
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Michael Ebenezer <span>1 day ago</span></h3>
+            <p>Clean code, great formatting, and excellent explanations here.</p>
+          </div>
+          <div className="comment-action">
+            <img src={like} alt="" />
+            <span>244</span>
+            <img src={dislike} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
