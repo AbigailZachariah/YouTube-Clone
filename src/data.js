998dacc48dda205c1de4bf4d939e7b5f1d571262
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-07-22 23:53:59 +0530

    creation of api key and updates

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const API_KEY=import.meta.env.VITE_YOUTUBE_API_KEY;
+
+//To change the number of views in terms of "nK" views 
+export const value_converter=(value)=> {
+    if(value>=1000000){
+        return Math.floor(value/1000000)+"M";
+    }
+    else if(value>=1000){
+        return Math.floor(value/1000)+"K";
+    }
+    else{
+        return value;
+    }
+}
