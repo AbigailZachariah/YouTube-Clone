1a63d12d256a5b276b0dcb4ea0acad5e327ba099
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-07-23 23:27:51 +0530

    Added the video page url

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './PlayVideo.css'
 import video1 from '../../assets/video.mp4'
 import like from '../../assets/like.png'
@@ -7,19 +7,36 @@ import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from'../../assets/user_profile.jpg'
+import { API_KEY, value_converter } from '../../data'
+import moment from 'moment'
 
-const PlayVideo = () => {
+const PlayVideo = ({videoId}) => {
+
+  const [apiData,setApiData]=useState(null);
+
+  const fetchVideoData =async() =>{
+    //Fetching videos data
+    const videoDetails_url=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+    await fetchVideoData(videoDetails_url).then(res=>res.json()).then(data=> setApiData(data.items[0]));
+  }
+
+  useEffect(()=>{
+    fetchVideoData();
+  },[])
+
   return (
     <div className='play-video'>
-      <video src={video1} controls autoplay muted></video>
-      <h3>Best YouTube Channel To Learn Web Development</h3>
+      {/* <video src={video1} controls autoplay muted></video> */}
+      <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      {/* "Title Here"=>if the title is not available */}
+      <h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
       <div className='play-video-info'>
-        <p>1525 Views &bull; 2 days ago</p>
+        <p>{apiData?value_converter(apiData.statistics.viewCount):"16K"} Views &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow():""}</p>
         <div>
-          <span><img src={like} alt="" />125</span>
+          <span><img src={like} alt="" />{apiData?value_converter(apiData.statistics.likeCount):155}</span>
           <span><img src={dislike} alt="" />2</span>
           <span><img src={share} alt="" />Share</span>
-          <span><img src={save} alt="" />Save</span>
+          <span><img src={save} alt="" />Save</span>               
         </div>
       </div>
       <hr />
